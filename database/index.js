60e8d3b269fac9e93121decfb013ca46a443fe9c
melanyvlm <melany.svalencia@gmail.com>
2025-02-03 14:08:01 -0500

    again everything 2

--- database/index.js
+++ database/index.js
@@ -1,35 +1,41 @@
-const { Pool } = require("pg")
-require("dotenv").config()
+const { Pool } = require("pg");
+require("dotenv").config();
+
 /* ***************
  * Connection Pool
  * SSL Object needed for local testing of app
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool
+let pool;
+
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl:  false
-  })
+    ssl: false, // Deshabilitar SSL en desarrollo
+  });
 
-// Added for troubleshooting queries
-// during development
-module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
-    } catch (error) {
-      console.error("error in query", { text })
-      throw error
-    }
-  },
-}
+  // Exportando objeto con la función de consulta para desarrollo
+  module.exports = {
+    async query(text, params) {
+      try {
+        const res = await pool.query(text, params);
+        console.log("executed query", { text });
+        return res;
+      } catch (error) {
+        console.error("error in query", { text });
+        throw error;
+      }
+    },
+  };
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
+    ssl: {
+      rejectUnauthorized: false, // Desactivar validación de certificados (útil en algunos entornos en la nube)
+    },
+  });
+
+  // Exportando el pool directamente para producción
+  module.exports = pool;
 }
